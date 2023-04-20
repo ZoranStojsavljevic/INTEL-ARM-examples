546df4524ebfd1400a7f39c97cf78e5fa49b9588
Zoran Stojsavljevic <zoran.stojsavljevic@gmail.com>
2023-04-20 07:27:38 +0200

    [security] fixed the beta path of cap_test.c to have a meaningfull output

--- Linux/security/cap_test/cap_test.c
+++ Linux/security/cap_test/cap_test.c
@@ -18,28 +18,37 @@
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
 #endif
 
+enum rv_id {
+	NO_ERROR = 0,
+	ERROR_GETRESUID,
+	ERROR_GETRESGID,
+	ERROR_CAP_SETUID,
+	ERROR_CAP_SETGROUPS,
+	ERROR_EXEC_FILE_2,
+	ERROR_EXEC_FILE_1
+};
+
 static int print_ids()
 {
-	int   rv;
-	gid_t egid, rgid, sgid;
-	uid_t euid, ruid, suid;
+	int	rv;
+	gid_t	egid, rgid, sgid;
+	uid_t	euid, ruid, suid;
 
 	rv = getresuid(&ruid, &euid, &suid);
 	if (0 != rv) {
 		printf("Unable to get UIDs: %s\n", strerror(errno));
-		goto out;
+		return ERROR_GETRESUID;
 	}
 	printf("RUID: % 5d, EUID: % 5d, SUID: % 5d\n", ruid, euid, suid);
 
 	rv = getresgid(&rgid, &egid, &sgid);
 	if (0 != rv) {
 		printf("Unable to get GIDs: %s\n", strerror(errno));
-		goto out;
+		return ERROR_GETRESGID;
 	}
 	printf("RGID: % 5d, EGID: % 5d, SGID: % 5d\n", rgid, egid, sgid);
 
-out:
-	return rv;
+	return NO_ERROR;
 }
 
 int main(int argc, char **argv)
@@ -58,28 +67,26 @@ int main(int argc, char **argv)
 	printf("Pid of '%s' is %d\n", name, pid);
 
 	rv = print_ids();
-	if (0 != rv) {
-		goto out;
-	}
+	if (0 != rv)
+		return rv;
 
 	printf("Setting UID to root user\n");
 	rv = cap_setuid((uid_t)0);
 	if (0 != rv) {
 		printf("Unable to set UID to root user: %s\n", strerror(errno));
-		goto out;
+		return ERROR_CAP_SETUID;
 	}
 	printf("Setting GID to root group\n");
 	rv = cap_setgroups((gid_t)0, 0, NULL);
 	if (0 != rv) {
 		printf("Unable to set GID to root group: %s\n", strerror(errno));
-		goto out;
+		return ERROR_CAP_SETGROUPS;
 	}
 
 	printf("Parent's RUID, EUID, SUID\n");
 	rv = print_ids();
-	if (0 != rv) {
-		goto out;
-	}
+	if (0 != rv)
+		return rv;
 
 	const char *exec_file1 = EXEC_FILE_1;
 	char *const exec_args1[] = { EXEC_FILE_1, NULL };
@@ -90,7 +97,7 @@ int main(int argc, char **argv)
 	printf("\n");
 
 	// Create a child process
-   	pid_t cpid = fork();
+	pid_t cpid = fork();
 	// This is the child process pid = 0
 	if (0 == cpid) {
 		const char *exec_file2 = EXEC_FILE_2;
@@ -102,15 +109,18 @@ int main(int argc, char **argv)
 		}
 		printf("\n");
 		printf("Child's RUID, EUID, SUID\n");
-		print_ids();
+		rv = print_ids();
+		if (0 != rv)
+			return rv;
 		rv = execv(exec_file2, exec_args2);
 		printf("Unable to execute process: '%s'\n", strerror(errno));
+		return ERROR_EXEC_FILE_2;
 	}
 	printf("Parent's RUID, EUID, SUID\n");
-	print_ids();
+	rv = print_ids();
+	if (0 != rv)
+		return rv;
 	rv = execv(exec_file1, exec_args1);
 	printf("Unable to execute process: '%s'\n", strerror(errno));
-
-out:
-	return rv;
+	return ERROR_EXEC_FILE_1;
 }
